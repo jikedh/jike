a209d9c9de6b1c2a1b371850fb5afe5c2b270f30
2026617199 <2026617199@qq.com>
2026-05-07 17:02:39 +0800

    测试一下会不会实现自动更新

--- src/renderer/pages/Sidebar/SidebarCeBianLan.tsx
+++ src/renderer/pages/Sidebar/SidebarCeBianLan.tsx
@@ -151,7 +151,7 @@ export const SidebarCeBianLan = () => {
           <SidebarNavItem
             id="model-settings"
             icon={<PanelRightOpen size={24} />}
-            label="模型管理"
+            label="模型管理(Test)"
             onClick={() => handleNavClick("/settings")}
           />
         </SidebarNav>
