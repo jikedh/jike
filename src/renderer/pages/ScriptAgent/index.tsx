a79ead1244909c71101ea159a86ef9c73d06ca4b
2026617199 <2026617199@qq.com>
2026-08-05 11:34:26 +0800

    新增剧本Agent入口及页面占位

---
+++ src/renderer/pages/ScriptAgent/index.tsx
@@ -0,0 +1,7 @@
+const ScriptAgentPage = () => (
+    <div className="flex h-full min-h-screen items-center justify-center bg-[#09090b] text-2xl text-white">
+        功能正在开发中
+    </div>
+);
+
+export default ScriptAgentPage;
